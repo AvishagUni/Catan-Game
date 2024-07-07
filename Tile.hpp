6f0e75ff3dd370a986af10b4038d0120b2b2a219
AvishagUni <AVISHAG.Minnes@msmail.ariel.ac.il>
2024-07-07 11:37:39 +0300

    Update Tile.hpp

--- Tile.hpp
+++ Tile.hpp
@@ -6,21 +6,30 @@
 
 using namespace std;
 
+enum class Resource
+{
+    BRICK,
+    GRAIN,
+    LUMBER,
+    ORE,
+    WOOL
+};
+
 class Tile
 {
     int id;
     int number;
-    string resource; 
+    Resource resource; 
     vector<Node *> nodes;
 
 public:
-    Tile(int i, int n, string r);
+    Tile(int i, int num, Resource res);
     ~Tile();
 
     void addNodes(const vector<Node *> &nodes2);
 
-    string getNumber() const;
-    string getResource() const;
+    int getNumber() const;
+    Resource getResource() const;
     int getId() const;
     vector<size_t> &getNodes() ;
 };
