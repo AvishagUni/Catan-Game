2c543a512e421129466b99a770084db5d320ca3f
AvishagUni <AVISHAG.Minnes@msmail.ariel.ac.il>
2024-07-07 11:36:00 +0300

    Update Edge.hpp

--- Edge.hpp
+++ Edge.hpp
@@ -1,9 +1,19 @@
+#include "Player.hpp"
+
 class Edge
 {
-    int owner;
+    int id;
+    Player* owner;
+    vector<Node *> adjNodes;
 
 public:
 
-    int getOwner();
-    void setOwner(int playerNumber);
+    Edge(int i) : id(i) {}
+    ~Edge();
+
+    Player* getOwner();
+    void setOwner(Player* o);
+
+    vector<Node *> getAdjSettlements();
+    void setAdjSettlements(Node* n1, Node* n2);
 };
