16547b5c9a8a468897e379e90bab1d835f9a3ab3
AvishagUni <AVISHAG.Minnes@msmail.ariel.ac.il>
2024-07-07 11:35:40 +0300

    Update Edge.cpp

--- Edge.cpp
+++ Edge.cpp
@@ -1,11 +1,37 @@
 #include "Edge.hpp"
 
-int Edge::getOwner()
+Edge::Edge(int i) : id(i)
+{
+    owner = nullptr;
+    this->adjNodes = {};
+}
+
+Edge::~Edge()
+{
+    for (int i = 0; i < 2; i++)
+    {
+        delete adjNodes[i];
+    }
+}
+
+
+Player* Edge::getOwner()
 {
     return owner;
 }
 
-void Edge::setOwner(int playerNumber)
+void Edge::setOwner(Player* o)
 {
-    owner = playerNumber;
+    owner = o;
+}
+
+vector<Node *> Edge::getAdjSettlements()
+{
+    return adjNodes;
+}
+
+void Edge::setAdjSettlements(Node* n1, Node* n2)
+{
+    adjNodes.push_back(n1);
+    adjNodes.push_back(n2);
 }
