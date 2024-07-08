bcee8a6d48fbba7d802b81eb023f0cdc0ea451d0
AvishagUni <AVISHAG.Minnes@msmail.ariel.ac.il>
2024-07-08 17:06:10 +0300

    Update Catan.hpp

--- Catan.hpp
+++ Catan.hpp
@@ -1,3 +1,5 @@
+// minnesav@gmail.com
+
 #ifndef CATAN_HPP
 #define CATAN_HPP
 
@@ -15,17 +17,19 @@ class Catan {
 
     public:
 
-    Catan(Player* p1, Player* p2, Player* p3);
-    ~Catan();
+    Catan(Player* p1, Player* p2, Player* p3); // Constructor
+    ~Catan(); // Destructor
 
-    void buildRoad(int i, Player* player, bool firstRound = false);   
-    void buildSettlement(int i, Player* player, bool firstRound = false);
-    void buildCity(int i, Player* player);
-    void useDevelopmentCard(Player* player);
-    void distributeResources(int diceRoll);
-    void checkWinCondition();
+    Board* getBoard(); // Get the board
 
-    void checkLargestArmy(); 
+    void buildRoad(int i, Player* player, bool firstRound = false); // Build a road   
+    void buildSettlement(int i, Player* player, bool firstRound = false); // Build a settlement
+    void buildCity(int i, Player* player); // Build a city
+    void useDevelopmentCard(Player* player); // Use a development card
+    void distributeResources(int diceRoll); // Distribute resources
+    void checkWinCondition(); // Check if a player has won
+
+    void checkLargestArmy();  // Check if a player has the largest army (3 knights)
 };
 
 #endif // CATAN_HPP
