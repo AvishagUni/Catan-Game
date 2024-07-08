dc3492c5d0ac35384c2a8ff36ad384b616f58ffe
AvishagUni <AVISHAG.Minnes@msmail.ariel.ac.il>
2024-07-08 10:46:19 +0300

    Update Tile.cpp

--- Tile.cpp
+++ Tile.cpp
@@ -4,13 +4,6 @@ Tile::Tile(int i, int num, Resource res) : id(i) , number(num), resource(res) {
     nodes = {0, 0, 0, 0, 0, 0};
 }
 
-Tile::~Tile()
-{
-    for(int i = 0; i < 6; i++)
-    {
-        delete nodes[i];
-    }
-}
 
 void Tile::addNodes(const vector<Node *> &nodes2)
 {
@@ -35,7 +28,7 @@ int Tile::getId() const
     return id;
 }
 
-vector<size_t> &Tile::getNodes()
+vector<size_t> Tile::getNodes()
 {
     vector<size_t> node_ids;
     for (int i = 0; i < 6; i++)
