03d16cac56e805a3651a0fac316575991de9f015
AvishagUni <AVISHAG.Minnes@msmail.ariel.ac.il>
2024-07-08 10:41:39 +0300

    Update Catan.hpp

--- Catan.hpp
+++ Catan.hpp
@@ -1,26 +1,31 @@
+#ifndef CATAN_HPP
+#define CATAN_HPP
+
 #include <vector>
 #include "Player.hpp"
-#include "Board.hpp"
 #include "Node.hpp"
 
 using namespace std;
 
+class Board;
+
 class Catan {
     vector<Player*> players;
-    Board board;
+    Board* board;
 
     public:
 
     Catan(Player* p1, Player* p2, Player* p3);
     ~Catan();
 
-    void buildRoad(int i, Player* player);   
-    void buildSettlement(int i, Player* player);
+    void buildRoad(int i, Player* player, bool firstRound = false);   
+    void buildSettlement(int i, Player* player, bool firstRound = false);
     void buildCity(int i, Player* player);
     void useDevelopmentCard(Player* player);
     void distributeResources(int diceRoll);
     void checkWinCondition();
 
     void checkLargestArmy(); 
-    void checkLongestRoad();
 };
+
+#endif // CATAN_HPP
