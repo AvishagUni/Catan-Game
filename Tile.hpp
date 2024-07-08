1c0d005c32fd97a12e57ad9306ac64329e5dc34c
AvishagUni <AVISHAG.Minnes@msmail.ariel.ac.il>
2024-07-08 16:59:10 +0300

    Update Tile.hpp

--- Tile.hpp
+++ Tile.hpp
@@ -1,19 +1,18 @@
+// minnesav@gmail.com
+
+#ifndef TILE_HPP
+#define TILE_HPP
+
 #include <string>
 #include <vector>
 #include <iostream>
 
+#include "Resource.hpp"
 #include "Node.hpp"
 
 using namespace std;
 
-enum class Resource
-{
-    BRICK,
-    GRAIN,
-    LUMBER,
-    ORE,
-    WOOL
-};
+class Node;
 
 class Tile
 {
@@ -23,13 +22,15 @@ class Tile
     vector<Node *> nodes;
 
 public:
-    Tile(int i, int num, Resource res);
-    ~Tile();
+    Tile(int i, int num, Resource res); // Constructor
+    ~Tile() = default; // Destructor
 
-    void addNodes(const vector<Node *> &nodes2);
+    void addNodes(const vector<Node *> &nodes2); // Add nodes to the tile
 
-    int getNumber() const;
-    Resource getResource() const;
-    int getId() const;
-    vector<size_t> &getNodes() ;
+    int getNumber() const; // Get the number of the tile
+    Resource getResource() const; // Get the resource of the tile
+    int getId() const; // Get the id of the tile
+    vector<size_t> getNodes() ; // Get the nodes of the tile
 };
+
+#endif // TILE_HPP
