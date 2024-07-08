2e58fee05927215945f6fde71489c0ff98260314
AvishagUni <AVISHAG.Minnes@msmail.ariel.ac.il>
2024-07-08 17:02:09 +0300

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -1,3 +1,5 @@
+// minnesav@gmail.com
+
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
 
@@ -22,40 +24,40 @@ class Player {
     size_t knights;
 
     public:
-    Player(string name);
-    ~Player();
+    Player(string name); // Constructor
+    ~Player(); // Destructor
 
-    bool canBuyRoad() const;
-    bool canBuySettlement() const;
-    bool canBuyCity() const;
-    bool canBuyDevelopmentCard() const;
-    void buyRoad(size_t idx);
-    void buySettlement(size_t idx);
-    void buyCity(size_t idx);
-    void buyDevelopmentCard();
+    bool canBuyRoad() const; // Check if the player can buy a road
+    bool canBuySettlement() const; // Check if the player can buy a settlement
+    bool canBuyCity() const; // Check if the player can buy a city
+    bool canBuyDevelopmentCard() const; // Check if the player can buy a development card
+    void buyRoad(size_t idx); // Buy a road
+    void buySettlement(size_t idx); // Buy a settlement
+    void buyCity(size_t idx); // Buy a city
+    void buyDevelopmentCard(); // Buy a development card
 
-    string getName();
-    const vector<size_t>& getResources() const;
-    const vector<size_t>& getSettlements() const;
-    const vector<size_t>& getCities() const;
-    const vector<size_t>& getRoads() const;
-    const vector<size_t>& getCards() const;
-    size_t getPoints() const;
-    size_t getKnights() const;
+    string getName(); // Get the name of the player
+    const vector<size_t>& getResources() const; // Get the resources of the player
+    const vector<size_t>& getSettlements() const; // Get the settlements of the player
+    const vector<size_t>& getCities() const; // Get the cities of the player
+    const vector<size_t>& getRoads() const; // Get the roads of the player
+    const vector<size_t>& getCards() const; // Get the cards of the player
+    size_t getPoints() const; // Get the points of the player
+    size_t getKnights() const; // Get the knights of the player
 
-    void modifyKnights(int amount); 
-    void modifyPoints(int amount);
-    void modifyResources(int amount, Resource resource);
-    void modifyCards(int amount, CardType card);
+    void modifyKnights(int amount); // Modify the knights of the player
+    void modifyPoints(int amount); // Modify the points of the player
+    void modifyResources(int amount, Resource resource); // Modify the resources of the player
+    void modifyCards(int amount, CardType card); // Modify the cards of the player
 
     size_t stealResource(Resource resource); // for monopoly card
-    void tradeWithPlayer(Player* player, const vector<size_t>& resourcesToGive, const vector<size_t>& resourcesToTake);
-    void tradeWithBank(Resource resource1, Resource resource2);
+    void tradeWithPlayer(Player* player, const vector<size_t>& resourcesToGive, const vector<size_t>& resourcesToTake); // for trade with player
+    void tradeWithBank(Resource resource1, Resource resource2); // for trade with bank 4:1
 
-    void addRoad(int i);
-    void addSettlement(int i);
+    void addRoad(int i); // Add a road
+    void addSettlement(int i); // Add a settlement
 
-    int rollDice() const;
+    int rollDice() const; // Roll the dice
 
 };
 
